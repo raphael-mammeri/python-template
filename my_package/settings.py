8f35b96324342384b5a3e59f984affe4b2d3ce4a
Mohamed Rafik Mammeri <mammeri.mohamed@gmail.com>
2023-01-07 19:10:26 +0100

    update

---
+++ my_package/settings.py
@@ -0,0 +1,14 @@
+import pathlib
+
+import pydantic
+
+
+class Settings(pydantic.BaseSettings):
+
+    class Config:
+        # .env variables must have following prefix
+        env_prefix = "MY_PACKAGE_"
+        env_file = pathlib.Path(__file__).parent / ".env"
+
+    path_workdir: pathlib.Path = pathlib.Path(__file__).parent.parent
+    path_docs: pathlib.Path = path_workdir / "docs"
